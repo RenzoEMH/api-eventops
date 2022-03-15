8782b707a56838ecd464912e66693725664a0a74
mrff88 <mrff88@gmail.com>
2022-03-15 17:09:07 -0500

    add token validation middleware to post and put routes in event.router.js

--- api/routes/event.router.js
+++ api/routes/event.router.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import { eventCtrl } from '../controllers/index.js';
+import { validateToken } from '../middlewares/index.js';
 
 const { getAllEvents, createEvent, updateEvent } = eventCtrl;
 
@@ -12,7 +13,7 @@ const eventRoutes = {
 };
 
 router.get(eventRoutes.GET_ALL, getAllEvents);
-router.post(eventRoutes.CREATE, createEvent);
-router.put(eventRoutes.UPDATE, updateEvent);
+router.post(eventRoutes.CREATE, validateToken, createEvent);
+router.put(eventRoutes.UPDATE, validateToken, updateEvent);
 
 export default router;
