d99c0c6a1161c58959fe3cee465f63cf8e11e511
mrff88 <mrff88@gmail.com>
2022-03-24 17:12:07 -0500

    add sale.router.js to routes directory

--- api/routes/index.js
+++ api/routes/index.js
@@ -1,5 +1,6 @@
 import eventRouter from './event.router.js';
 import slideRouter from './slide.router.js';
-import userRouter from './user.router.js'
+import userRouter from './user.router.js';
+import saleRouter from './sale.router.js';
 
-export { eventRouter, slideRouter, userRouter };
+export { eventRouter, slideRouter, userRouter, saleRouter };
