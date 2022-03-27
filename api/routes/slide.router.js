12adfe09409bb7d4e0b61b4279c9d106d36072f4
mrff88 <mrff88@gmail.com>
2022-03-27 09:24:37 -0500

    add endpoints to slide.router.js

--- api/routes/slide.router.js
+++ api/routes/slide.router.js
@@ -2,20 +2,42 @@ import express from 'express';
 import { slideCtrl } from '../controllers/index.js';
 import { validateToken } from '../middlewares/index.js';
 
-const { getAllSlides, createSlide, updateSlide, deleteSlide } = slideCtrl;
+const {
+  getAllSlides,
+  createSlide,
+  updateAllOrdersCreateOne,
+  updateSlide,
+  updateAllSlides,
+  deleteSlide,
+  updateAllOrdersDeleteOne,
+} = slideCtrl;
 
 const router = express.Router();
 
 const slideRoutes = {
   GET_ALL: '/slides',
   CREATE: '/slides/create',
+  UPDATE_ALL_ORDERS_CREATE: '/slides/reorder-create',
   UPDATE: '/slides/update/:id',
+  UPDATE_ALL: '/slides/update-all/:id',
   DELETE: '/slides/delete/:id',
+  UPDATE_ALL_ORDERS_DELETE: '/slides/reorder-delete/:id',
 };
 
 router.get(slideRoutes.GET_ALL, getAllSlides);
 router.post(slideRoutes.CREATE, validateToken, createSlide);
+router.post(
+  slideRoutes.UPDATE_ALL_ORDERS_CREATE,
+  validateToken,
+  updateAllOrdersCreateOne
+);
 router.put(slideRoutes.UPDATE, validateToken, updateSlide);
+router.put(slideRoutes.UPDATE_ALL, validateToken, updateAllSlides);
 router.delete(slideRoutes.DELETE, validateToken, deleteSlide);
+router.delete(
+  slideRoutes.UPDATE_ALL_ORDERS_DELETE,
+  validateToken,
+  updateAllOrdersDeleteOne
+);
 
 export default router;
